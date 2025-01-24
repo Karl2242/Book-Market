def52e3b1f497644e57816c1f8516d5fa21b4de7
Karl2242 <kchikhicloud@gmail.com>
2025-01-24 14:12:43 +0100

    ADD : main

--- public/mainpage.php
+++ public/mainpage.php
@@ -2,6 +2,35 @@
 include_once "../utils/autoloader.php";
 session_start();
 
-var_dump($_SESSION["user"]);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book-Market</title>
+    <link rel="stylesheet" href="./assets/css/output.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    
+<header class="bg-[#011936] px-6 w-[100%] p-3 items-center flex gap-3">
+    <div class="w-[50%]">
+<img src="./assets/imgs/logo 1.png" alt="">
+<p class="font-inder text-white font-extralight">BOOK-MARKET</p>
+</div>
+
+<div class="flex gap-5 justify-end w-[50%]">
+<input class="border-solid text-black shadow-inner pl-4 rounded-lg border-2 border-[#77AD78] w-[37%]" type="text" placeholder="Rechercher">
+<a href=""><img src="./assets/imgs/card 1.png" alt=""></a>
+<a href=""><img src="./assets/imgs/user 1.png" alt=""></a>
+</div>
+</header>
+
+
+
+</body>
+</html>
 
